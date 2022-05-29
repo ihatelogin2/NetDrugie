431e8ab3577eacd8817c9ab589522d9d99d9b722
Kacper Bury <ihatelogin2@gmail.com>
2022-05-29 12:55:40 +0200

    Add new files

---
+++ ConsoleApp28052022Test/PESELWalidatorTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28052022Test
+{
+    
+    internal class PESELWalidatorTest
+    {
+        [Test]
+        public void PlecTest()
+        {
+            
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+namespace ConsoleApp28052022
+{
+    public class PESELWalidator
+    {
+        public static void Main(string[] args)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+            Console.WriteLine("Type any pesel:  ");
+
+            String pesel2 = Console.ReadLine();
+            Console.WriteLine(PESELValidator(pesel2));
+            Console.WriteLine(Plec(pesel2));
+            Console.WriteLine(SumaKontrolna(pesel2, weights));           
+
+        }
+        protected int[] wagi = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+        protected int[] pesel = { 0, 0, 2, 1, 2, 3, 0, 6, 1, 7, 0 };
+
+        public static bool PESELValidator(String pesel)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            bool result = false;
+            if (pesel.Length == 11)
+            {
+                int controlSum = SumaKontrolna(pesel, weights);
+                int controlNum = controlSum % 10;
+                controlNum = 10 - controlNum;
+                if (controlNum == 10)
+                {
+                    controlNum = 0;
+                }
+                int lastDigit = int.Parse(pesel[pesel.Length - 1].ToString());
+                result = controlNum == lastDigit;
+            }
+            return result;
+
+        }
+
+
+
+        private static int SumaKontrolna(string pesel, int[] weights)
+        {
+            int offset = 0;
+            int controlSum = 0;
+            for (int i = 0; i < pesel.Length - 1; i++)
+            {
+                controlSum += weights[i + offset] * int.Parse(pesel[i].ToString());
+            }
+            return controlSum;
+        }
+
+        public String DataUrodzenia(string pesel)
+        {
+            return pesel;
+        }
+
+        private static String Plec(string pesel)
+        {
+            if (pesel[9] % 2 == 1)
+            {
+                return "Mezczyzna";
+            }
+            else
+            {
+                return "Kobieta";
+            }
+
+        }
+
+
+        public Boolean SprawdzPesel()
+        {
+            return false;
+        }
+
+    }
+}
